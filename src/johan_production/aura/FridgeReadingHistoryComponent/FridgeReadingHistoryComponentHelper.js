112885ef26f5c83b34b286912e4f98eadc5bbe47
Johan Karlsteen <johan.karlsteen@eins.se>
2017-10-14 14:18:34 +0200

    Commiting build johan_production/152 to git

--- src/johan_production/aura/FridgeReadingHistoryComponent/FridgeReadingHistoryComponentHelper.js
+++ src/johan_production/aura/FridgeReadingHistoryComponent/FridgeReadingHistoryComponentHelper.js
@@ -2,41 +2,17 @@
  * Created by Johan Karlsteen on 2017-10-08.
  */
 ({
-        addData : function(chart, label, data) {
-            chart.data.labels.push(label);
-            chart.data.datasets.forEach((dataset) => {
-                dataset.data.push(data);
-            });
-        },
-        removeData : function(chart) {
-            chart.data.labels.pop();
-            chart.data.datasets.forEach((dataset) => {
-                dataset.data.pop();
-            });
+        addData : function(chart, labels, data) {
+            chart.data.labels = labels;
+            chart.data.datasets[0] = data[0];
+            chart.data.datasets[1] = data[1];
         },
         redrawData : function(component, event, helper, readings, chart, datasets) {
-            helper.removeData(chart);
             helper.addData(chart, readings.ts, datasets);
-            console.log(chart.data);
-            chart.update();
+            chart.update(0);
         },
         displayData : function(component, event, helper, readings) {
-            var datasets = [{
-                               label: 'Temperature',
-                               backgroundColor: 'red',
-                               borderColor: 'red',
-                               data: readings.temperature,
-                               yAxisID: "y-axis-1",
-                               fill: false,
-                           },
-                           {
-                               label: 'Humidity',
-                               backgroundColor: 'blue',
-                               borderColor: 'blue',
-                               data: readings.humidity,
-                               yAxisID: "y-axis-2",
-                               fill: false,
-                           }];
+            var datasets = [readings.temperature, readings.humidity];
             var chart = window.myLine;
             if(chart != null) {
                 helper.redrawData(component,event,helper,readings, chart, datasets);
@@ -45,7 +21,22 @@
                 type: 'line',
                 data: {
                     labels: readings.ts,
-                    datasets: datasets
+                    datasets: [{
+                                 label: 'Temperature',
+                                 backgroundColor: 'red',
+                                 borderColor: 'red',
+                                 data: readings.temperature,
+                                 yAxisID: "y-axis-1",
+                                 fill: false,
+                             },
+                             {
+                                 label: 'Humidity',
+                                 backgroundColor: 'blue',
+                                 borderColor: 'blue',
+                                 data: readings.humidity,
+                                 yAxisID: "y-axis-2",
+                                 fill: false,
+                             }]
                 },
                 options: {
                     responsive: true,
@@ -85,6 +76,8 @@
             window.myLine = new Chart(ctx, config);
         },
     refreshData : function(component,event,helper) {
+        var spinner = component.find('spinner');
+        $A.util.removeClass(spinner, "slds-hide");
         var action = component.get("c.getFridgeReadings");
         var endDate = component.get("v.today");
         var results = component.get("v.results");
@@ -93,26 +86,15 @@
         	results : results,
         	endDate : endDate
     	});
-    	console.log(action.getParams());
         action.setCallback(this, function(response){
             var state = response.getState();
             if (state === "SUCCESS") {
                 var fridgereadings = JSON.parse(response.getReturnValue());
                 helper.displayData(component,event,helper,fridgereadings);
             }
+            var spinner = component.find('spinner');
+            $A.util.addClass(spinner, "slds-hide");
         });
         $A.enqueueAction(action);
-
-        action = component.get("c.getAverageFridgeReadings");
-        action.setParams({
-        	deviceId : "2352fc042b6dc0ee"
-    	});
-        action.setCallback(this, function(response){
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                logger.debug('SUCCESS');
-            }
-        });
-        //$A.enqueueAction(action);
     }
 })
