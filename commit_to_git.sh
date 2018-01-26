9422dfdf8b01f36a2a4cb8f343a32cc5e80ec644
Johan Karlsteen <johan.karlsteen@gmail.com>
2018-01-26 14:43:59 +0100

    changed path to git

--- commit_to_git.sh
+++ commit_to_git.sh
@@ -1,5 +1,5 @@
 # Make sure your build environment sets GIT_PATH and other environment variables before executing this script
 set +x
-${GIT_PATH} config user.email "${GIT_USER_EMAIL}"
-${GIT_PATH} config user.name "${GIT_USER_NAME}"
-echo `${GIT_PATH} add src/${SF_ENVIRONMENTNAME} -A && ${GIT_PATH} commit -am "Commiting build ${SF_ENVIRONMENTNAME}/${BUILD_ID} to git"`
+git config user.email "${GIT_USER_EMAIL}"
+git config user.name "${GIT_USER_NAME}"
+echo `git add src/${SF_ENVIRONMENTNAME} -A && git commit -am "Commiting build ${SF_ENVIRONMENTNAME}/${BUILD_ID} to git"`
